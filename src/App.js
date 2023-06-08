36673d6b6a1bf4e52bf2b5936c0f010047465472
Linh <2115229@dlu.edu>
2023-06-09 01:04:07 +0700

    len di em

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import InfoLayout from './Layouts/InfoLayout';
 import StageLayout from './Layouts/StageLayout';
+import AccountLayout from './Layouts/AccountLayout';
 
 function App() {
-  return (
-    <div className="App">
-      <InfoLayout info="Nhập thông tin cây trồng" name="Tên cây trồng" stage="Nhập các giai đoạn phát triển cây trồng">
-        Thêm mới quy trình phát triển cây trồng
-      </InfoLayout>
-      <StageLayout info="Nhập thông tin giai đoạn cây trồng" name="Tên giai đoạn" content>
-        Thêm mới quy trình phát triển cây trồng
-      </StageLayout>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*} <InfoLayout
+                info="Nhập thông tin cây trồng"
+                name="Tên cây trồng"
+                stage="Nhập các giai đoạn phát triển cây trồng"
+            >
+                Thêm mới quy trình phát triển cây trồng
+            </InfoLayout>
+            <StageLayout info="Nhập thông tin giai đoạn cây trồng" name="Tên giai đoạn" content>
+                Thêm mới quy trình phát triển cây trồng
+            </StageLayout> {*/}
+            <AccountLayout>Cài đặt</AccountLayout>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Layouts/AccountLayout/index.js
@@ -0,0 +1,113 @@
+import Header from '../InfoLayout/Header';
+import styles from './AccountLayout.module.css';
+import classNames from 'classnames/bind';
+import Button from '../InfoLayout/Button';
+
+const cx = classNames.bind(styles);
+
+function AccountLayout(props) {
+    return (
+        <>
+            <Header>{props.children}</Header>
+            <div className={styles.container}>
+                <h1 className={styles.formname}>Thông tin tài khoản</h1>
+                <div className={styles.wrap}>
+                    <div className={styles.avatarwrap}>
+                        <div className={styles.titlewrap}>
+                            <p className={styles.lefttext}>Ảnh đại diện</p>
+                            <p className={styles.righttext}>Chỉnh sửa</p>
+                        </div>
+                        <div className={styles.avatar}></div>
+                    </div>
+                    <div className={styles.userinfowrap}>
+                        <div className={styles.titlewrap}>
+                            <p className={styles.lefttext}>Thông tin cá nhân</p>
+                            <p className={styles.righttext}>Chỉnh sửa</p>
+                        </div>
+                        <div className={styles.userformwrap}>
+                            <div className={styles.leftform}>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Họ và tên</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Địa chỉ</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Ngày sinh</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Giới tính</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                            </div>
+                            <div className={styles.rightform}>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Số điện thoại</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Email</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>CCCD</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.wrap}>
+                    <div className={cx('userinfowrap', 'marright')}>
+                        <div className={styles.titlewrap}>
+                            <p className={styles.lefttext}>Thông tin tài khoản</p>
+                        </div>
+                        <div className={styles.userformwrap}>
+                            <div className={styles.leftform}>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Tên đăng nhập</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Mật khẩu đăng nhập</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Email đăng ký</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('userinfowrap', 'marleft')}>
+                        <div className={styles.titlewrap}>
+                            <p className={styles.lefttext}>Đổi mật khẩu</p>
+                        </div>
+                        <div className={styles.userformwrap}>
+                            <div className={styles.leftform}>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Mật khẩu hiện tại</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Mật khẩu mới</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                                <div className={styles.item}>
+                                    <p className={styles.label}>Xác nhận mật khẩu mới</p>
+                                    <input className={styles.textbox} type="text"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Button></Button>
+        </>
+    );
+}
+
+export default AccountLayout;
