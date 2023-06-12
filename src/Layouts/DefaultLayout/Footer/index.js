dd6b552242b907329bac1cd288cee5724c628f3c
Linh <2115229@dlu.edu>
2023-06-12 15:24:47 +0700

    len di em

--- src/Layouts/DefaultLayout/Footer/index.js
+++ src/Layouts/DefaultLayout/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <div className={styles.footer}>
-            <p className={styles.text}>Copyright Nhóm 6 Quản lý quy trình phát triển cây trồng vật nuôi</p>
+            <p className={styles.text}>Copyright © Nhóm 6 Quản lý quy trình phát triển cây trồng vật nuôi</p>
         </div>
     );
 }
